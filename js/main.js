ea37aa746ab066d045a589ccd730ecaad8111ba2
Matteojk95 <matteopalmer2095@gmail.com>
2023-07-04 19:46:20 +0200

    new

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-for (let i=0; i<=100; i++) {
+for (let i=1; i<=100; i++) {
     document.getElementById("squaresContainer").innerHTML+= `<div class="square">
        <p class="text-center text-white fs-3 fw-3 vertical-align-square">${i}</p>
        </div>`
@@ -6,7 +6,7 @@ for (let i=0; i<=100; i++) {
 }
  
 // Scrivi un programma che stampi i numeri da 1 a 100
-for (let i=0; i<=100; i++) {
+for (let i=1; i<=100; i++) {
 
 // per i multipli di 3 stampi “Fizz” al posto del numero 
 if (i % 3 == 0) {
