b5b424a3443fc7a7d2a4a334faec04ffea487eea
Matteojk95 <matteopalmer2095@gmail.com>
2023-07-04 16:28:03 +0200

    problema con java script a fare apparire i 100

---
+++ js/main.js
@@ -0,0 +1,9 @@
+for (let i=0; i<100; i++) {
+    document.getElementById("squarescontainer").innerHTML+= `<div class="square">
+       <p class="text-center text-white fs-3 fw-3 vertical-align-square">${i}</p>
+       </div>`
+       ;     
+}
+
+
+
